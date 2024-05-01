7602f74f60b0c9bf5d7718bad6b789404bf0f639
renzxgrayGit <renzocabajar9@gmail.com>
2024-05-01 20:15:54 +0800

    front-end

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
-
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#24262b",
+        "primary-gray": "#5e626f;",
+        "primary-green": "#0D775E;",
+        "red-gray": "#444444",
+        "white-100": "#F3F3F3",
+      },
+    },
+  },
+  plugins: [],
+};
